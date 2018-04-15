a5b3e66373de9601dd051ec48e42fe712cef288e
Matison <matison14@gmail.com>
2018-04-16 01:39:49 +0200

    Initial commit

---
+++ Assets/Scripts/Kamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kamera : MonoBehaviour {
+
+	public GameObject Gracz;
+	public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void LastUpdate () {
+		transform.position = Gracz.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float speed;
+	public Rigidbody rb;
+	public Text countText;
+	public Text winText;
+
+	private int count;
+	// Update is called once per frame
+	void Start () {
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void Update () {
+		float poziom = Input.GetAxis ("Horizontal");
+		float pion = Input.GetAxis ("Vertical");
+		Vector3 ruch = new Vector3 (poziom, 0.0f, pion);
+		//Rigidbody.AddForce (ruch * speed * Time.deltaTime);
+		GetComponent<Rigidbody>().AddForce(ruch*speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+
+		}
+		if (other.gameObject.CompareTag ("Pick Up2"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 4;
+			SetCountText ();
+
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Punkty: " + count.ToString ();
+		if (count >= 9)
+		{
+			winText.text = "Wygrałeś!";
+		}
+	}
+}
